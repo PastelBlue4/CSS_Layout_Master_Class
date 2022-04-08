10e6dce9cf976d197112757d587d09facf64a8b9
Romuru <pastelblue0721@gmail.com>
2022-04-08 23:50:39 +0900

    data fething

--- chall_6/src/script.js
+++ chall_6/src/script.js
@@ -1,6 +1,1 @@
-const testbutton2 = document
-  .getElementById("btn")
-  .addEventListener("click", () => {
-    console.log(testbutton2);
-    testbutton2.innerText = "soriee";
-  });
+// hi there!
